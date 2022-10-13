c2f022b9bff47f16726f5752fbfc1b4f4926884a
CRafaelS <carlos.rafael@engenharia.ufjf.br>
2022-10-13 18:39:24 -0300

    feat:  Número de estudantes estudando no mesmo horário. Req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
+from numpy import empty
+
+
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+    count_students = 0
+    if not target_time:
+        return None
+    for entry_hour, exits_hour in permanence_period:
+        if type(entry_hour) != int or type(exits_hour) != int:
+            return None
+        if entry_hour <= target_time <= exits_hour:
+            count_students += 1
+    return count_students
